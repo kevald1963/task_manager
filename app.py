3e0186ab80e772c72374f3e55312f7dbf8d7836a
kevald1963 <kevin_alderson@yahoo.co.uk>
2019-12-11 21:19:42 +0000

    Created base and task html template files. Added Materialize 'accordion' functionality to templates.

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
 
+app.config["MONGO_URI"] = os.getenv("MONGO_URI_TASK_MANAGER")
+app.config["MONGO_DBNAME"] = "task_manager"
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return "Hello world!"
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'),
