c0838b385447f697bbd2e90ca15d2ca105155cc5
kevald1963 <kevin_alderson@yahoo.co.uk>
2019-12-19 17:30:13 +0000

    Form fields now populated from Mongo EXCEPT that the Due Date won't show for some reason. Needs further investigation.

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config["MONGO_DBNAME"] = "task_manager"
 
 mongo = PyMongo(app)
 
-gitpod_url = "https://5000-ae34fd21-0471-4da9-8255-f8e6a8ea7825.ws-eu01.gitpod.io/"
+gp_url = "https://5000-ae34fd21-0471-4da9-8255-f8e6a8ea7825.ws-eu01.gitpod.io/"
 
 @app.route('/')
 @app.route('/get_tasks')
@@ -27,15 +27,18 @@ def insert_task():
     # Convert form data to a dictionary to make it usable by Mongo.
     tasks.insert_one(request.form.to_dict())
     # Have to use hard-coded gitpod url, otherwise tries to redirect to localhost causing a connection refused error.
-    return redirect(gitpod_url + 'get_tasks')
+    return redirect(gp_url + 'get_tasks')
     #return redirect(url_for('get_tasks'))
 
 @app.route('/edit_task/<task_id>')
 def edit_task(task_id):
-    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
-    all_categories = mongo.db.catagories.find()
+    # Fetch the task record from the database for the displayed task we want to edit.
+    _task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    # Fetch all categories from the database to display in the dropdown list, so user can choose a new one if they wish.
+    _categories = mongo.db.categories.find()
+    category_list = [category for category in _categories]
     # Display the record for editing.
-    return render_template("edittask.html", task=the_task, categories=all_categories)
+    return render_template("edittask.html", task= _task, categories = category_list)
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'),
